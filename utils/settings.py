99619f3aaa1ab4ae43a44aeb3b5029f73d086d53
Manoj Nathwani <manoj_1993@hotmail.com>
2018-10-31 19:10:49 +0000

    removed mysqlclient

--- utils/settings.py
+++ utils/settings.py
@@ -1,2 +1,2 @@
-DATABASE_CONNECTION_URL = 'mysql+mysqldb://eahub@eahub:password_5rlvsk959wV4iTR908ScAPPiThGY7URBOZv0hx2Ref2NJvjE9OAH4ky77dHI@eahub.mysql.database.azure.com/eahub'
+DATABASE_CONNECTION_URL = 'mysql://eahub@eahub:password_5rlvsk959wV4iTR908ScAPPiThGY7URBOZv0hx2Ref2NJvjE9OAH4ky77dHI@eahub.mysql.database.azure.com/eahub'
 PAGINATION_SIZE = 100
