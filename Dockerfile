3050237f0f81c97c9834ba216e3ce0f07ca44c3e
Manoj Nathwani <manoj_1993@hotmail.com>
2018-12-15 19:30:01 +0000

    added recaptcha to signup

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM python:3.6
 RUN mkdir -p /code
 WORKDIR /code
 
-RUN pip install gunicorn django psycopg2-binary whitenoise
+RUN pip install django==2.1.4 gunicorn==19.9.0 psycopg2-binary==2.7.6.1 pytz==2018.7 whitenoise==4.1.2 requests==2.21.0
 
 COPY . /code/
 
--- users/views.py
+++ users/views.py
@@ -1,11 +1,26 @@
+import os
+
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render
+import requests
 
 from .models import Profile
 from .forms import ProfileCreationForm
+
+
+def recaptcha_valid(recaptcha_response):
+    request = requests.post(
+        'https://www.google.com/recaptcha/api/siteverify',
+        {
+            'secret': os.environ['RECAPTCHA_PRIVATE_KEY'],
+            'response': recaptcha_response
+        }
+    ).json()
+    return request.get('success', False)
+
 
 class SignUp(generic.CreateView):
     template_name = 'registration/signup.html'    
@@ -15,8 +30,12 @@ class SignUp(generic.CreateView):
         valid = super(SignUp, self).form_valid(form)
         email, password = form.cleaned_data.get('email'), form.cleaned_data.get('password1')
         new_user = authenticate(email=email, password=password)
-        login(self.request, new_user)
+        if recaptcha_valid(self.request.POST.get('g-recaptcha-response')):
+            login(self.request, new_user)
+        else:
+            new_user.delete() # delete recaptcha failing user made by robot
         return valid
+            
 
 @login_required(login_url=reverse_lazy('login'))
 def ProfileView(request):
