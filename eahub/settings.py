81bf5e95e1967ed5c202c70ded96d8b8bca75ac0
Manoj Nathwani <manoj_1993@hotmail.com>
2018-12-15 15:54:26 +0000

    integrated password reset flow

--- eahub/settings.py
+++ eahub/settings.py
@@ -126,5 +126,10 @@ LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'index'
 LOGIN_URL = '/users/login/'
 
-EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
-EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
+EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'EA Hub <noreply@eahub.org>'
