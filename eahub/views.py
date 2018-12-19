4be37bf0ab19c391ef796daa44a383449f7b4617
Sebastian <sebbecker@gmx.net>
2018-12-19 13:53:02 +0000

    add toggle

--- eahub/views.py
+++ eahub/views.py
@@ -21,38 +21,15 @@ def index(request):
             )
         + '},'
         for x in rows
-    ])    
+    ])
     return render(request, 'eahub/index.html', {
         'map_data': map_data
     })
 
 def profiles(request):
-    rows = Profile.objects\
+    rows_groups = Group.objects\
         .exclude(lat__isnull=True)
-    map_data = ''.join([
-        '{' +
-            'lat: {lat}, lng: {lon}, label:"{name}", path: "{path}"'.format(
-                lat=str(x.lat),
-                lon=str(x.lon),
-                name=' '.join([x.first_name, x.last_name]),
-                path='/{obj}/{id}'.format(
-                    obj='profile',
-                    id=x.id
-                )
-            )
-        + '},'
-        for x in rows
-    ])    
-    return render(request, 'eahub/profiles.html', {
-        'page_name': 'Profiles',
-        'profiles': rows,
-        'map_data': map_data
-    })
-
-def groups(request):
-    rows = Group.objects\
-        .exclude(lat__isnull=True)
-    map_data = ''.join([
+    map_data_groups = ''.join([
         '{' +
             'lat: {lat}, lng: {lon}, label:"{name}", path: "{path}"'.format(
                 lat=str(x.lat),
@@ -64,10 +41,67 @@ def groups(request):
                 )
             )
         + '},'
-        for x in rows
-    ])    
+        for x in rows_groups
+    ])
+    rows_profiles = Profile.objects\
+        .exclude(lat__isnull=True)
+    map_data_profiles = ''.join([
+        '{' +
+            'lat: {lat}, lng: {lon}, label:"{name}", path: "{path}"'.format(
+                lat=str(x.lat),
+                lon=str(x.lon),
+                name=' '.join([x.first_name, x.last_name]),
+                path='/{obj}/{id}'.format(
+                    obj='profile',
+                    id=x.id
+                )
+            )
+        + '},'
+        for x in rows_profiles
+    ])
+    return render(request, 'eahub/profiles.html', {
+        'page_name': 'Profiles',
+        'profiles': rows_profiles,
+        'map_data_groups': map_data_groups,
+        'map_data_profiles': map_data_profiles
+    })
+
+def groups(request):
+    rows_groups = Group.objects\
+        .exclude(lat__isnull=True)
+    map_data_groups = ''.join([
+        '{' +
+            'lat: {lat}, lng: {lon}, label:"{name}", path: "{path}"'.format(
+                lat=str(x.lat),
+                lon=str(x.lon),
+                name=x.name,
+                path='/{obj}/{id}'.format(
+                    obj='group',
+                    id=x.id
+                )
+            )
+        + '},'
+        for x in rows_groups
+    ])
+    rows_profiles = Profile.objects\
+        .exclude(lat__isnull=True)
+    map_data_profiles = ''.join([
+        '{' +
+            'lat: {lat}, lng: {lon}, label:"{name}", path: "{path}"'.format(
+                lat=str(x.lat),
+                lon=str(x.lon),
+                name=' '.join([x.first_name, x.last_name]),
+                path='/{obj}/{id}'.format(
+                    obj='profile',
+                    id=x.id
+                )
+            )
+        + '},'
+        for x in rows_profiles
+    ])
     return render(request, 'eahub/groups.html', {
         'page_name': 'Groups',
-        'groups': rows,
-        'map_data': map_data
+        'groups': rows_groups,
+        'map_data_groups': map_data_groups,
+        'map_data_profiles': map_data_profiles
     })
