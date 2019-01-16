06ebbd28d9144197dd81d9f2be5a48633dfc6ed7
Manoj Nathwani <manoj_1993@hotmail.com>
2019-01-16 21:27:44 +0000

    rewrote front-end

--- eahub/static/js/banner.js
+++
@@ -1,5 +0,0 @@
-var menu_burger = document.getElementById('menu_burger');
-var drop_down_links = document.getElementById('drop_down_links');
-menu_burger.onclick = function() {
-  drop_down_links.style.visibility = drop_down_links.style.visibility == "visible" ? "hidden" : "visible";
-}
