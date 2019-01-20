6e48f1dc5a0d06b98848f76f981129aa88b61b81
Manoj Nathwani <manoj_1993@hotmail.com>
2019-01-20 10:46:51 +0000

    set gdpr_confirmed=True on signup

--- profiles/models.py
+++ profiles/models.py
@@ -17,7 +17,7 @@ class Profile(AbstractUser):
     lon = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True, verbose_name='longitude')
     city_or_town = models.CharField(max_length=100, null=True, blank=True)
     country = models.CharField(max_length=100, null=True, blank=True)
-    gdpr_confirmed = models.BooleanField(default=False)
+    gdpr_confirmed = models.BooleanField(default=True)
 
     def __str__(self):
         return self.full_name()
