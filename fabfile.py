a81bc1a3b58af52b92ad7183af989146cebd6577
Manoj Nathwani <manoj_1993@hotmail.com>
2019-01-28 17:28:39 +0000

    updated fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -2,12 +2,16 @@ from fabric.api import local
 from fabric import operations
 
 def build():
+    local('docker-compose build')
     local('docker build -t eahub:latest .')
     local('y | docker system prune')
 
 def run():
     local('docker-compose run --service-ports web') #  with ipdb support
     # local('docker-compose up') #  without ipdb support
+    
+def migrate():
+    local('docker-compose run web django-admin migrate')
 
 def deploy():
     build()
