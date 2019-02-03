769f490b99b0dbd8a89564e2198942c3f0713b4a
Sebastian <sebbecker@gmx.net>
2019-02-03 13:57:01 +0000

    update btn only appears when both boxes checked

--- eahub/static/scripts/edit.js
+++ eahub/static/scripts/edit.js
@@ -1,7 +1,18 @@
-var privacy1_btn = document.getElementById('privacy-1')
-var privacy2_btn = document.getElementById('privacy-2')
+var privacy1_checkbox = document.getElementById('privacy-1')
+var privacy2_checkbox = document.getElementById('privacy-2')
+var update_btn = document.getElementById('update-btn')
 
-privacy1_btn.addEventListener('RadioStateChange', function() {
-  console.log('change')
-  console.log(privacy1_btn)
+privacy1_checkbox.addEventListener('click', function() {
+  toggleUpdateBtn()
 })
+privacy2_checkbox.addEventListener('click', function() {
+  toggleUpdateBtn()
+})
+
+function toggleUpdateBtn() {
+  if (privacy1_checkbox.checked && privacy2_checkbox.checked) {
+    update_btn.style.display = 'block'
+  } else {
+    update_btn.style.display = 'none'
+  }
+}
