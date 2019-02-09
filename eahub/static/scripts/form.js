a35aec9c42bbca3a5da8ab0b66459cbf8f3d9813
Sebastian <sebbecker@gmx.net>
2019-02-09 13:51:22 +0000

    add multi select

--- eahub/static/scripts/form.js
+++ eahub/static/scripts/form.js
@@ -1,2 +1,4 @@
+$('.multiselect-form').multiselect();
+
 var edit_checkbox_cause_area_other = document.getElementById('edit-checkbox-cause-area-other')
 var textarea_cause_area_other = document.getElementById('id_cause_areas_other')
