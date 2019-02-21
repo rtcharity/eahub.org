ecf8bf61403320aef0f530c2f626df1b31892d5b
Taymon A. Beal <taymonbeal@gmail.com>
2019-02-21 18:44:49 -0500

    Revert "Send email notifications of broken links"
    Signed-off-by: Taymon A. Beal <taymon@google.com>

--- eahub/config/settings.py
+++ eahub/config/settings.py
@@ -43,7 +43,6 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.BrokenLinkEmailsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
