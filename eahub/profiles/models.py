159465e48e1d7fb9717eb2f41b22f5e4c784ad5f
Sebastian <sebbecker@gmx.net>
2019-03-01 08:17:48 +0000

    303 org affiliation profiles (#354)
    Closes #303
    
    Signed-off-by: Sebastian <sebbecker@gmx.net>

--- eahub/profiles/models.py
+++ eahub/profiles/models.py
@@ -68,6 +68,74 @@ class GivingPledge(enum.Enum):
         ONE_FOR_THE_WORLD: "One for the World",
     }
 
+class OrganisationalAffiliation(enum.Enum):
+
+    EIGHTY_THOUSAND_HOURS = 1
+    ANIMAL_CHARITY_EVALUATORS = 2
+    BERKELEY_EXISTENTIAL_RISK_INITIATIVE = 3
+    CENTER_FOR_APPLIED_RATIONALITY = 4
+    CENTER_FOR_HUMAN_COMPATIBLE_AI = 5
+    CENTRE_FOR_EFFECTIVE_ALTRUISM = 6
+    CENTRE_FOR_THE_STUDY_OF_EXISTENTIAL_RISK = 7
+    CHARITY_ENTREPRENEURSHIP = 8
+    CHARITY_SCIENCE_HEALTH = 9
+    FORETHOUGHT_FOUNDATION = 10
+    FOUNDATIONAL_RESEARCH_INSTITUTE = 11
+    FOUNDERS_PLEDGE = 12
+    FUTURE_OF_LIFE_INSTITUTE = 13
+    GIVEWELL = 14
+    GLOBAL_CATASTROPHIC_RISK_INSTITUTE = 15
+    GLOBAL_PRIORITIES_INSTITUTE = 16
+    LEVERHULME_CENTER_FOR_THE_FUTURE_OF_INTELLIGENCE = 17
+    LOCAL_EFFECTIVE_ALTRUISM_NETWORK = 18
+    MACHINE_INTELLIGENCE_RESEARCH_INSTITUTE = 19
+    ONE_FOR_THE_WORLD = 20
+    OPEN_PHILANTHROPY_PROJECT = 21
+    RAISING_FOR_EFFECTIVE_GIVING = 22
+    RETHINK_CHARITY = 23
+    RETHINK_CHARITY_FORWARD = 24
+    RETHINK_PRIORITIES = 25
+    SENTIENCE_INSTITUTE = 26
+    STIFTUNG_FUR_EFFEKTIVEN_ALTRUISMUS = 27
+    STUDENTS_FOR_HIGH_IMPACT_CHARITY = 28
+    THE_GOOD_FOOD_INSTITUTE = 29
+    THE_LIFE_YOU_CAN_SAVE = 30
+    WILD_ANIMAL_INITIATIVE = 31
+
+    labels = {
+        EIGHTY_THOUSAND_HOURS: "80,000 Hours",
+        ANIMAL_CHARITY_EVALUATORS: "Animal Charity Evaluators",
+        BERKELEY_EXISTENTIAL_RISK_INITIATIVE: "Berkeley Existential Risk Initiative",
+        CENTER_FOR_APPLIED_RATIONALITY: "Center for Applied Rationality",
+        CENTER_FOR_HUMAN_COMPATIBLE_AI: "Center for Human Compatible AI",
+        CENTRE_FOR_EFFECTIVE_ALTRUISM: "Centre for Effective Altruism",
+        CENTRE_FOR_THE_STUDY_OF_EXISTENTIAL_RISK: "Centre for the Study of Existential Risk",
+        CHARITY_ENTREPRENEURSHIP: "Charity Entrepreneurship",
+        CHARITY_SCIENCE_HEALTH: "Charity Science Health",
+        FORETHOUGHT_FOUNDATION: "Forethought Foundation",
+        FOUNDATIONAL_RESEARCH_INSTITUTE: "Foundational Research Institute",
+        FOUNDERS_PLEDGE: "Founders Pledge",
+        FUTURE_OF_LIFE_INSTITUTE: "Future of Life Institute",
+        GIVEWELL: "GiveWell",
+        GLOBAL_CATASTROPHIC_RISK_INSTITUTE: "Global Catastrophic Risk Institute",
+        GLOBAL_PRIORITIES_INSTITUTE: "Global Priorities Institute",
+        LEVERHULME_CENTER_FOR_THE_FUTURE_OF_INTELLIGENCE: "Leverhulme Center for the Future of Intelligence",
+        LOCAL_EFFECTIVE_ALTRUISM_NETWORK: "Local Effective Altruism Network",
+        MACHINE_INTELLIGENCE_RESEARCH_INSTITUTE: "Machine Intelligence Research Institute",
+        ONE_FOR_THE_WORLD: "One for the World",
+        OPEN_PHILANTHROPY_PROJECT: "Open Philanthropy Project",
+        RAISING_FOR_EFFECTIVE_GIVING: "Raising for Effective Giving",
+        RETHINK_CHARITY: "Rethink Charity",
+        RETHINK_CHARITY_FORWARD: "Rethink Charity Forward",
+        RETHINK_PRIORITIES: "Rethink Priorities",
+        SENTIENCE_INSTITUTE: "Sentience Institute",
+        STIFTUNG_FUR_EFFEKTIVEN_ALTRUISMUS: "Stiftung für Effektiven Altruismus (EAF)",
+        STUDENTS_FOR_HIGH_IMPACT_CHARITY: "Students for High Impact Charity",
+        THE_GOOD_FOOD_INSTITUTE: "The Good Food Institute",
+        THE_LIFE_YOU_CAN_SAVE: "The Life You Can Save",
+        WILD_ANIMAL_INITIATIVE: "Wild Animal Initiative"
+    }
+
 
 class Profile(models.Model):
 
@@ -90,6 +158,9 @@ class Profile(models.Model):
         enum.EnumField(ExpertiseArea), blank=True, default=list
     )
     available_as_speaker = models.BooleanField(null=True, blank=True, default=None)
+    organisational_affiliations = postgres_fields.ArrayField(
+        enum.EnumField(OrganisationalAffiliation), blank=True, default=list
+    )
     summary = models.TextField(blank=True)
     giving_pledges = postgres_fields.ArrayField(
         enum.EnumField(GivingPledge), blank=True, default=list
@@ -129,6 +200,12 @@ class Profile(models.Model):
         else:
             return "N/A"
 
+    def get_pretty_organisational_affiliations(self):
+        if self.organisational_affiliations:
+            return ", ".join(map(OrganisationalAffiliation.label, self.organisational_affiliations))
+        else:
+            return "N/A"
+
     def csv(self, response):
         writer = csv.writer(response)
         writer.writerows(
@@ -143,6 +220,7 @@ class Profile(models.Model):
                 ["open_to_job_offers", self.open_to_job_offers],
                 ["expertise_areas", self.get_pretty_expertise()],
                 ["available_as_speaker", self.available_as_speaker],
+                ["organisational_affiliations", self.get_pretty_organisational_affiliations()],
                 ["summary", self.summary],
                 ["giving_pledges", self.get_pretty_giving_pledges()],
             ]
