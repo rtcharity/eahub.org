6b517cfcdab5bc78a9dc7671810b457caa8787d7
Sebastian <sebbecker@gmx.net>
2019-03-05 23:36:39 +0000

    Allow users to request to claim groups (#359)
    Closes #149.
    
    Signed-off-by: Sebastian <sebbecker@gmx.net>
    Signed-off-by: Taymon A. Beal <taymon@google.com>

--- eahub/localgroups/urls.py
+++ eahub/localgroups/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     urls.path("new", views.LocalGroupCreateView.as_view(), name="localgroups_create"),
+    urls.path('<slug:slug>/claim', views.claim_group, name='claim_group'),
     urls.path("<slug:slug>", views.LocalGroupDetailView.as_view(), name="group"),
     urls.path(
         "<slug:slug>/edit",
--- eahub/localgroups/views.py
+++ eahub/localgroups/views.py
@@ -3,6 +3,15 @@ from django.contrib.auth import mixins as auth_mixins
 from django.views.generic import detail as detail_views
 from django.views.generic import edit as edit_views
 from rules.contrib import views as rules_views
+from django.urls import reverse_lazy
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.core.mail import mail_managers
+from django.contrib.sites.shortcuts import get_current_site
+from django.contrib import messages
+from django.template.loader import render_to_string
+from django.views.decorators.http import require_POST
+from django.http import HttpResponse
 
 from .forms import LocalGroupForm
 from .models import LocalGroup
@@ -48,3 +57,26 @@ class LocalGroupDeleteView(rules_views.PermissionRequiredMixin, edit_views.Delet
     template_name = "eahub/delete_group.html"
     success_url = urls.reverse_lazy('groups')
     permission_required = 'localgroups.delete_local_group'
+
+
+@login_required
+@require_POST
+def claim_group(request, slug):
+    group = get_object_or_404(LocalGroup, slug=slug)
+    subject = "EA Group claimed: {0}".format(group.name)
+    try:
+        user_eahub_url = "https://{0}/profile/{1}".format(get_current_site(request).domain,request.user.profile.slug)
+    except Profile.DoesNotExist:
+        user_eahub_url = "about:blank"
+    message = render_to_string('emails/claim_group.html', {
+        'user_eahub_url': user_eahub_url,
+        'user_name': request.user.profile.name,
+        'group_name': group.name,
+        'user_email': request.user.email
+    })
+    mail_managers(subject, message)
+    messages.success(
+        request,
+        ''' Thank you, we have received your request to claim this group. Our admin team will send you an email once they have checked your request. ''',
+    )
+    return redirect('/group/{}'.format(group.slug))
