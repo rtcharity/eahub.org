5162b3847cfd811d3a0c19bdf5b0e032899e9d05
Sebastian <sebbecker@gmx.net>
2019-03-06 21:20:41 +0000

    move JS for group.html into separate file
    Signed-off-by: Sebastian <sebbecker@gmx.net>

--- eahub/base/static/scripts/main.js
+++ eahub/base/static/scripts/main.js
@@ -47,27 +47,3 @@ var navbar = document.getElementById('navbar')
 menu_btn.addEventListener('click', function() {
   navbar.style.display = navbar.style.display == 'inline-block' ? 'none' : 'inline-block';
 })
-
-let claim_group = {
-  toggle_btn: document.getElementById('claim_group_toggle'),
-  confirm_field: document.getElementById('claim_group_confirm_field'),
-  togglers: document.getElementsByClassName('claim_group_toggler')
-}
-
-let report_group_inactive = {
-  toggle_btn: document.getElementById('report_group_inactive_toggle'),
-  confirm_field: document.getElementById('report_group_inactive_confirm_field'),
-  togglers: document.getElementsByClassName('report_group_inactive_toggler')
-}
-
-function toggleButton(obj) {
-  for (let toggler of obj.togglers) {
-    toggler.addEventListener('click', function() {
-      obj.confirm_field.style.display = obj.confirm_field.style.display == 'block' ? 'none' : 'block';
-      obj.toggle_btn.style.display = obj.toggle_btn.style.display == 'none' ? 'inline-block' : 'none';
-    })
-  }
-}
-
-toggleButton(claim_group)
-toggleButton(report_group_inactive)
