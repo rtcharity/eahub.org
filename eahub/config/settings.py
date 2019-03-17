34a20161d7935fe49b99c3268410cec12c5a0a9f
Taymon A. Beal <taymon@google.com>
2019-03-17 13:38:37 -0700

    Show X-Forwarded-For header in broken link emails
    This will hopefully help get us more informative ones.
    
    Part of #321.
    
    Signed-off-by: Taymon A. Beal <taymon@google.com>

--- eahub/config/settings.py
+++ eahub/config/settings.py
@@ -47,7 +47,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.BrokenLinkEmailsMiddleware",
+    "eahub.base.middleware.BrokenLinkEmailsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
