3500a017fcc5f632a01f1280fafdbaf48122a2a5
Taymon A. Beal <taymonbeal@gmail.com>
2019-03-17 13:41:33 -0700

    Fix password change form problems (#442)
    Specifically:
    - Use Bootstrap 3 form templates (this was always supposed to be the case, it was a settings bug)
    - Override help text template to use a <div> so that there can be <ul>s in it
    - Fix the title
    
    Fixes: #441
    
    Signed-off-by: Taymon A. Beal <taymon@google.com>

--- eahub/config/build_settings.py
+++ eahub/config/build_settings.py
@@ -23,6 +23,3 @@ SECRET_KEY = b"build_secret_key"
 STATIC_ROOT = "/static/"
 STATIC_URL = "/static/"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
-# django-crispy-forms
-CRISPY_TEMPLATE_PACK = "bootstrap4"
