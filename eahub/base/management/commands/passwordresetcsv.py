cf5457416d31739cd940bba419d1de3e085b7616
Taymon A. Beal <taymonbeal@gmail.com>
2019-03-25 10:34:35 -0700

    Create management command to generate mass password reset CSV (#493)
    Also extend password reset expiration to 30 days.
    
    Part of #421.
    
    Signed-off-by: Taymon A. Beal <taymon@google.com>

---
+++ eahub/base/management/commands/passwordresetcsv.py
@@ -0,0 +1,38 @@
+import csv
+
+from django.conf import settings
+from django.contrib.auth import tokens
+from django.core.management import base
+from django import urls
+from django.utils import encoding
+from django.utils import http
+
+from ....profiles import models
+
+
+class Command(base.BaseCommand):
+    help = "Outputs a CSV of reset links for passwordless users to stdout"
+
+    def handle(self, *args, **options):
+        self.stdout.ending = None
+        writer = csv.writer(self.stdout)
+        writer.writerow(["Email Address", "Name", "One-Time Login Link"])
+        for profile in models.Profile.objects.select_related("user").filter(
+            user__password="", user__is_active=True
+        ):
+            user = profile.user
+            writer.writerow(
+                [
+                    user.email,
+                    profile.name,
+                    urls.reverse(
+                        "password_reset_confirm",
+                        kwargs={
+                            "uidb64": http.urlsafe_base64_encode(
+                                encoding.force_bytes(user.pk)
+                            ),
+                            "token": tokens.default_token_generator.make_token(user),
+                        },
+                    ),
+                ]
+            )
