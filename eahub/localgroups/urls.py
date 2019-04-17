1bf2519e09848a7e0c00db19cf851025c30dbc0f
Taymon A. Beal <taymonbeal@gmail.com>
2019-04-17 04:36:04 -0400

    Use trailing slashes consistently in URLs (#628)
    Personally I prefer not having a trailing slash, but a bunch of other Django stuff we're using includes them, and inconsistencies here are likely to lead to subtle bugs.
    
    URLs with file extensions (favicon.ico and robots933456.txt) don't have trailing slashes.
    
    Fixes: #518
    
    Signed-off-by: Taymon A. Beal <taymon@google.com>

--- eahub/localgroups/urls.py
+++ eahub/localgroups/urls.py
@@ -4,17 +4,17 @@ from . import views
 
 
 urlpatterns = [
-    urls.path("new", views.LocalGroupCreateView.as_view(), name="localgroups_create"),
-    urls.path('<slug:slug>/claim', views.claim_group, name='claim_group'),
-    urls.path('<slug:slug>/report-inactive', views.report_group_inactive, name='report_group_inactive'),
-    urls.path("<slug:slug>", views.LocalGroupDetailView.as_view(), name="group"),
+    urls.path("new/", views.LocalGroupCreateView.as_view(), name="localgroups_create"),
+    urls.path('<slug:slug>/claim/', views.claim_group, name='claim_group'),
+    urls.path('<slug:slug>/report-inactive/', views.report_group_inactive, name='report_group_inactive'),
+    urls.path("<slug:slug>/", views.LocalGroupDetailView.as_view(), name="group"),
     urls.path(
-        "<slug:slug>/edit",
+        "<slug:slug>/edit/",
         views.LocalGroupUpdateView.as_view(),
         name="localgroups_update",
     ),
     urls.path(
-        "<slug:slug>/delete",
+        "<slug:slug>/delete/",
         views.LocalGroupDeleteView.as_view(),
         name="localgroups_delete",
     ),
