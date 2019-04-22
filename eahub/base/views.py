56ca933514af74904108ba68e446a393e944994a
Taymon A. Beal <taymonbeal@gmail.com>
2019-04-22 04:38:20 -0400

    Redirect site features from legacy URLs (#639)
    Fixes: #607
    
    Signed-off-by: Taymon A. Beal <taymon@google.com>

--- eahub/base/views.py
+++ eahub/base/views.py
@@ -122,6 +122,10 @@ class FaviconView(base.RedirectView):
         return static.static("favicon.ico")
 
 
+class LegacyRedirectView(base.RedirectView):
+    permanent = True
+
+
 def healthCheck(request):
     return HttpResponse(status=204)
 
