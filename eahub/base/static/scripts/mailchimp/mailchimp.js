d240ab81ae35b7d5dfc8946f5ba50a59158dbc45
Sebastian <sebbecker@gmx.net>
2019-04-25 14:10:21 +0100

    648 mailchimp signup (#655)
    Fixes #648
    
    - Adds signup form for mailchimp newsletter
    - Email required, first and last name optional. Or do we only want a "name" field as our user models don't distinguish between first and last names?
    - We might want to add some text about the newsletter ("We'll send you monthly updates about the Hub blablabla"

---
+++ eahub/base/static/scripts/mailchimp/mailchimp.js
@@ -0,0 +1,1 @@
+(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='MMERGE3';ftypes[3]='text';fnames[4]='MMERGE4';ftypes[4]='text';fnames[5]='MMERGE5';ftypes[5]='text';}(jQuery));var $mcj = jQuery.noConflict(true);
