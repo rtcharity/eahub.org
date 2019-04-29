ba8f3573db3a587dc0df78ba01f62f51a8b629e3
Sebastian Becker <sebbecker@gmx.net>
2019-04-29 20:35:33 +0100

    Move cookieconsent scripts into extra folder

---
+++ eahub/base/static/scripts/cookieconsent/init.js
@@ -0,0 +1,15 @@
+window.addEventListener("load", function(){
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#635274"
+    },
+    "button": {
+      "background": "#A7DBAB",
+      "text": "white"
+    }
+  },
+  "content": {
+    "href": "https://eahub.org/privacy-policy"
+  }
+})});
