26e9a7ccbf632ce4afd6d9a346ba7a83ac096e50
Sebastian Becker <sebbecker@gmx.net>
2019-04-30 20:48:02 +0100

    Remove full stop

--- eahub/config/settings.py
+++ eahub/config/settings.py
@@ -195,7 +195,7 @@ RECAPTCHA_PUBLIC_KEY = env.str("RECAPTCHA_SITE_KEY")
 CRISPY_TEMPLATE_PACK = "bootstrap3"
 
 # Django PWNED Passwords
-PWNED_VALIDATOR_ERROR = "Your password is not secure enough."
+PWNED_VALIDATOR_ERROR = "Your password is not secure enough"
 PWNED_VALIDATOR_FAIL_SAFE = False
 
 
