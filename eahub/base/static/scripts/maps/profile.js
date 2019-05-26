00721e17b8395bbdd074177a01e675256e4bbf69
Sebastian Becker <sebbecker@gmx.net>
2019-05-26 22:09:11 +0100

    Fix error in Profile class

--- eahub/base/static/scripts/maps/profile.js
+++ eahub/base/static/scripts/maps/profile.js
@@ -1,9 +1,9 @@
 class Profile {
-  constructor(location, mapType) {
+  constructor(location, type) {
     this.label = location.label != undefined ? location.label : undefined
     this.anonymous = location.label != undefined ? false : true
     this.path = location.path != undefined ? location.path : undefined
-    this.active = mapType == 'groups' ? true : undefined
+    this.active = type == 'groups' ? location.active : undefined
   }
 }
 
