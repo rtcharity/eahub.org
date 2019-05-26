7a722ffca78acdaa191869691d41427607b1d256
Taymon A. Beal <taymon@google.com>
2019-05-26 09:12:27 -0400

    Fully working pytest setup

--- eahub/base/tests/smoke_test.py
+++ eahub/base/tests/smoke_test.py
@@ -1,7 +1,6 @@
 import pytest
 
 @pytest.mark.nondestructive
-def test_homepage_title(live_server):
-    return
+def test_homepage_title(driver, live_server):
     driver.get(live_server.url)
     assert driver.title == "EA Hub Home"
