f2086c12c905f4382b9b176590be863f278f5eb8
Sebastian Becker <sebbecker@gmx.net>
2019-05-27 20:57:01 +0100

    Add unit tests for addDescription

--- eahub/base/static/scripts/spec/javascripts/marker_spec.js
+++ eahub/base/static/scripts/spec/javascripts/marker_spec.js
@@ -1,11 +1,11 @@
 describe('Marker', function() {
-  let marker, mapMock, profilesMock, locationMock
+  let marker, mapMock, profilesMock
   beforeEach(function() {
     mapMock = {
       google: googleMock,
       type: 'groups'
     }
-    locationMock = {lat: 50, lng: 0}
+
   })
   afterEach(function() {
     mapMock = null
@@ -13,31 +13,49 @@ describe('Marker', function() {
 
   describe('constructor', function(){
     it('creates googleMarker whose label shows the number of profiles linked to marker', function() {
-      profilesMock = [activeProfileMock, activeProfileMock]
-      marker = new Marker(mapMock, locationMock, profilesMock)
+      marker = new Marker(mapMock, londonLatLng, [london1Mock, london2Mock])
 
       expect(marker.googleMarker.label.text).toBe('2')
     })
     it('creates googleMarker whose icon is active if some profiles in it are active', function() {
-      profilesMock = [activeProfileMock, activeProfileMock]
-      marker = new Marker(mapMock, locationMock, profilesMock)
+      marker = new Marker(mapMock, londonLatLng, [london1Mock, london2Mock])
 
       expect(marker.googleMarker.icon.url).not.toContain('inactive')
       expect(marker.googleMarker.icon.url).toContain('active')
     })
     it('creates googleMarker whose icon is active if map type is individuals', function() {
-      profilesMock = [activeProfileMock, activeProfileMock]
       mapMock.type = 'individuals'
-      marker = new Marker(mapMock, locationMock, profilesMock)
+      marker = new Marker(mapMock, londonLatLng, [london1Mock, london2Mock])
 
       expect(marker.googleMarker.icon.url).not.toContain('inactive')
       expect(marker.googleMarker.icon.url).toContain('active')
     })
     it('creates googleMarker whose icon is inactive if all profiles in it are inactive', function() {
-      profilesMock = [inactiveProfileMock, inactiveProfileMock]
-      marker = new Marker(mapMock, locationMock, profilesMock)
+      marker = new Marker(mapMock, londonLatLng, [london2Mock])
 
       expect(marker.googleMarker.icon.url).toContain('inactive')
+    })
+  })
+  describe('addDescription', function() {
+    it('adds list of profiles to marker', function() {
+      marker = new Marker(mapMock, londonLatLng, [london1Mock, london2Mock])
+      marker.addDescription()
+
+      expect(marker.googleMarker.desc).toContain('London1')
+      expect(marker.googleMarker.desc).toContain('London2')
+    })
+    it('adds links to profiles to marker', function() {
+      marker = new Marker(mapMock, londonLatLng, [london1Mock, london2Mock])
+      marker.addDescription()
+
+      expect(marker.googleMarker.desc).toContain('london-1')
+      expect(marker.googleMarker.desc).toContain('london-2')
+    })
+    it('adds that groups is inactive to marker', function() {
+      marker = new Marker(mapMock, londonLatLng, [london1Mock, london2Mock])
+      marker.addDescription()
+
+      expect(marker.googleMarker.desc).toContain('London2 (inactive)')
     })
   })
 })
