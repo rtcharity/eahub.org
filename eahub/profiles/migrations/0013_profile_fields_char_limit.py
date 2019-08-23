9cf40ec8fccfa9f2c981e1dc40346be234c4ce25
Sebastian <sebbecker@gmx.net>
2019-08-24 00:14:16 +0100

    724 char limit on free text in profiles (#821)
    Fixes: #724

---
+++ eahub/profiles/migrations/0013_profile_fields_char_limit.py
@@ -0,0 +1,38 @@
+import django.core.validators
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [("profiles", "0012_profile_links")]
+
+    operations = [
+        migrations.AlterField(
+            model_name="profile",
+            name="cause_areas_other",
+            field=models.TextField(
+                blank=True, validators=[django.core.validators.MaxLengthValidator(2000)]
+            ),
+        ),
+        migrations.AlterField(
+            model_name="profile",
+            name="expertise_areas_other",
+            field=models.TextField(
+                blank=True, validators=[django.core.validators.MaxLengthValidator(2000)]
+            ),
+        ),
+        migrations.AlterField(
+            model_name="profile",
+            name="summary",
+            field=models.TextField(
+                blank=True, validators=[django.core.validators.MaxLengthValidator(2000)]
+            ),
+        ),
+        migrations.AlterField(
+            model_name="profile",
+            name="topics_i_speak_about",
+            field=models.TextField(
+                blank=True, validators=[django.core.validators.MaxLengthValidator(2000)]
+            ),
+        ),
+    ]
