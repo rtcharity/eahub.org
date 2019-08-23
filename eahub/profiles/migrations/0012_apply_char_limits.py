189e99f1fb87ed0f159b9c5aac4483f5fd7203ef
Sebastian Becker <sebbecker@gmx.net>
2019-08-23 16:44:33 +0100

    formatting

--- eahub/profiles/migrations/0012_apply_char_limits.py
+++ eahub/profiles/migrations/0012_apply_char_limits.py
@@ -3,29 +3,27 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('profiles', '0011_profile_career_interest_areas'),
-    ]
+    dependencies = [("profiles", "0011_profile_career_interest_areas")]
 
     operations = [
         migrations.AlterField(
-            model_name='profile',
-            name='cause_areas_other',
+            model_name="profile",
+            name="cause_areas_other",
             field=models.TextField(blank=True, max_length=2000),
         ),
         migrations.AlterField(
-            model_name='profile',
-            name='expertise_areas_other',
+            model_name="profile",
+            name="expertise_areas_other",
             field=models.TextField(blank=True, max_length=2000),
         ),
         migrations.AlterField(
-            model_name='profile',
-            name='summary',
+            model_name="profile",
+            name="summary",
             field=models.TextField(blank=True, max_length=2000),
         ),
         migrations.AlterField(
-            model_name='profile',
-            name='topics_i_speak_about',
+            model_name="profile",
+            name="topics_i_speak_about",
             field=models.TextField(blank=True, max_length=2000),
         ),
     ]
