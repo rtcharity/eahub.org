60f624d9bce10f961a05a72234548b7105892936
Sebastian Becker <sebbecker@gmx.net>
2020-02-22 14:08:36 +0000

    formatting

--- eahub/profiles/migrations/0015_profile_open_to_couchsurfers.py
+++ eahub/profiles/migrations/0015_profile_open_to_couchsurfers.py
@@ -5,14 +5,12 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('profiles', '0014_profile_is_approved'),
-    ]
+    dependencies = [("profiles", "0014_profile_is_approved")]
 
     operations = [
         migrations.AddField(
-            model_name='profile',
-            name='open_to_couchsurfers',
+            model_name="profile",
+            name="open_to_couchsurfers",
             field=models.BooleanField(blank=True, default=None, null=True),
-        ),
+        )
     ]
