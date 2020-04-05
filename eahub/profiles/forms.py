12cb2f3fdf78ce3b2184f286087ff76f0da640e6
Sebastian Becker <sebbecker@gmx.net>
2020-04-05 15:02:11 +0100

    linter

--- eahub/profiles/forms.py
+++ eahub/profiles/forms.py
@@ -32,9 +32,7 @@ class SignupForm(forms.Form):
     def signup(self, request, user):
         is_public = self.cleaned_data["is_public"]
         name = self.cleaned_data["name"]
-        Profile.objects.create(
-            user=user, is_public=is_public, name=name
-        )
+        Profile.objects.create(user=user, is_public=is_public, name=name)
 
 
 class EditProfileForm(forms.ModelForm):
@@ -117,11 +115,7 @@ class EditProfileCommunityForm(forms.ModelForm):
 
     class Meta:
         model = Profile
-        fields = (
-            "available_as_speaker",
-            "topics_i_speak_about",
-            "local_groups",
-        )
+        fields = ("available_as_speaker", "topics_i_speak_about", "local_groups")
         widgets = {
             "topics_i_speak_about": forms.Textarea(attrs={"rows": 3, "maxlength": 2000})
         }
