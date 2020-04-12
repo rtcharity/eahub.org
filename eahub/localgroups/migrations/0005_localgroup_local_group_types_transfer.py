528b2f4511a49c745215bc0536ce75ff3cedddf2
Sebastian Becker <sebbecker@gmx.net>
2020-04-12 16:33:34 +0100

    Add local_group_types and migrate old data

--- eahub/localgroups/migrations/0005_localgroup_local_group_types_transfer.py
+++ eahub/localgroups/migrations/0005_localgroup_local_group_types_transfer.py
@@ -6,6 +6,13 @@ from django.db import migrations
 import django_enumfield.db.fields
 import eahub.localgroups.models
 
+def copy_group_type(apps, schema_editor):
+    LocalGroup = apps.get_model("localgroups", "LocalGroup")
+    for row in LocalGroup.objects.all():
+        if row.local_group_type:
+            row.local_group_types=[row.local_group_type]
+            row.save()
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -17,5 +24,6 @@ class Migration(migrations.Migration):
             model_name="localgroup",
             name="local_group_types",
             field=django.contrib.postgres.fields.ArrayField(base_field=django_enumfield.db.fields.EnumField(default=1, enum=eahub.localgroups.models.LocalGroupType), blank=True, default=list, size=None),
-        )
+        ),
+        migrations.RunPython(copy_group_type)
     ]
