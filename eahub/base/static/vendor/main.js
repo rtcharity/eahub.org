9f9ffcff8260fe1e4d96e615469e978eaba53b2b
Sebastian <sebbecker@gmx.net>
2020-06-14 14:32:28 +0100

    Fix fontawesome

--- eahub/base/static/vendor/main.js
+++ eahub/base/static/vendor/main.js
@@ -3,33 +3,19 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 import 'cookieconsent/build/cookieconsent.min.css';
 
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
 
 import { library, dom } from '@fortawesome/fontawesome-svg-core';
-import { faGlobe } from '@fortawesome/free-solid-svg-icons';
-import { faUserAstronaut } from '@fortawesome/free-solid-svg-icons';
-import { faSignInAlt } from '@fortawesome/free-solid-svg-icons';
-import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
-import { faUsers } from '@fortawesome/free-solid-svg-icons';
-import { faBook } from '@fortawesome/free-solid-svg-icons';
-import { faExchangeAlt } from '@fortawesome/free-solid-svg-icons';
-import { faInfo } from '@fortawesome/free-solid-svg-icons';
-import { faComment } from '@fortawesome/free-solid-svg-icons';
-import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import { faGlobe, faUserAstronaut, faSignInAlt, faUserPlus, faUser, faUsers, faBook, faExchangeAlt,
+         faInfo, faComment, faSignOutAlt, faPencilAlt, faEnvelope, faSitemap, faBed, faFlag, faPlus,
+         faLock, faMap, faDownload, faTrash, faEnvelopeSquare } from '@fortawesome/free-solid-svg-icons';
 
-
-library.add(faUserAstronaut);
-library.add(faSignInAlt);
-library.add(faUserPlus);
-library.add(faUser);
-library.add(faUsers);
-library.add(faGlobe);
-library.add(faBook);
-library.add(faExchangeAlt);
-library.add(faInfo);
-library.add(faComment);
-library.add(faSignOutAlt);
-
+library.add(faUserAstronaut, faSignInAlt, faUserPlus, faUser, faUsers, faGlobe, faBook, faExchangeAlt,
+faInfo, faComment, faSignOutAlt, faPencilAlt, faEnvelope, faSitemap, faFlag, faBed, faPlus, faLock, faMap,
+faDownload, faTrash, faEnvelopeSquare);
 
 // Replace any existing <i> tags with <svg>
 dom.i2svg();
