e2bc4c3518d6e8231cbd88738f9d31c61bfc4699
Sebastian <sebbecker@gmx.net>
2020-07-12 14:50:18 +0100

    factor out ExportCSVMixin

--- eahub/base/utils.py
+++ eahub/base/utils.py
@@ -1,3 +1,6 @@
+import csv
+from django.http import HttpResponse
+
 from ..profiles import models
 
 
@@ -7,3 +10,21 @@ def user_display(user):
     except models.Profile.DoesNotExist:
         return user.email
     return profile.name
+
+
+class ExportCsvMixin:
+    def export_csv(self, request, queryset, ignore):
+        meta = models.LocalGroup._meta
+        field_names = [field.name for field in meta.fields if field.name not in ignore]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow(obj.convert_to_row(field_names))
+
+        return response
+
+    export_csv.short_description = "Export as CSV"
--- eahub/localgroups/admin.py
+++ eahub/localgroups/admin.py
@@ -1,28 +1,13 @@
 from django.contrib import admin
 
-import csv
-from django.http import HttpResponse
-
 from . import models
+from ..base import utils
 
-class LocalGroupAdmin(admin.ModelAdmin):
+class LocalGroupAdmin(admin.ModelAdmin, utils.ExportCsvMixin):
     actions = ['export_csv']
 
     def export_csv(self, request, queryset):
-        meta = models.LocalGroup._meta
-        field_names = [field.name for field in meta.fields if field.name != "local_group_type"]
-
-        response = HttpResponse(content_type='text/csv')
-        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
-        writer = csv.writer(response)
-
-        writer.writerow(field_names)
-        for obj in queryset:
-            row = writer.writerow(obj.convert_to_row(field_names))
-
-        return response
-
-    export_csv.short_description = "Export as CSV"
+        return utils.ExportCsvMixin.export_csv(self,request,queryset,["local_group_type"])
 
 
 admin.site.register(models.LocalGroup, LocalGroupAdmin)
