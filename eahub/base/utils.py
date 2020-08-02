a89a1908519c4bff2f3ad570dfd2ab3bc5935cb6
Sebastian <sebbecker@gmx.net>
2020-08-02 16:26:45 +0100

    formatting

--- eahub/base/utils.py
+++ eahub/base/utils.py
@@ -15,7 +15,11 @@ def user_display(user):
 
 class ExportCsvMixin:
     def export_csv(self, request, queryset, meta, ignore=[]):
-        field_names = [field.name for field in meta.fields + meta.many_to_many if field.name not in ignore]
+        field_names = [
+            field.name
+            for field in meta.fields + meta.many_to_many
+            if field.name not in ignore
+        ]
         response = HttpResponse(content_type="text/csv")
         response["Content-Disposition"] = "attachment; filename={}.csv".format(meta)
         writer = csv.writer(response)
