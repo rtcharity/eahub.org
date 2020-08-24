95467d603be2be837792daa04a047ac63b567e35
Sebastian <sebbecker@gmx.net>
2020-08-24 22:43:42 +0100

    linter

--- eahub/profiles/admin.py
+++ eahub/profiles/admin.py
@@ -9,10 +9,7 @@ class ProfileAdmin(admin.ModelAdmin, utils.ExportCsvMixin):
 
     def export_csv(self, request, queryset):
         meta = models.Profile._meta
-        fieldnames = [
-            field.name
-            for field in meta.fields + meta.many_to_many
-        ]
+        fieldnames = [field.name for field in meta.fields + meta.many_to_many]
         return utils.ExportCsvMixin.export_csv(
             self, request, queryset, fieldnames, "profiles"
         )
