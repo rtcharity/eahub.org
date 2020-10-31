7dde6305b10bf5c706842d2fd566aaeccc9a73af
Sebastian <sebbecker@gmx.net>
2020-10-31 19:45:04 +0000

    more formatting

--- eahub/tests/test_localgroups_model.py
+++ eahub/tests/test_localgroups_model.py
@@ -1,17 +1,18 @@
 from django.test import TestCase
-from eahub.localgroups.models import LocalGroup, Organisership
+
 from eahub.base.models import User
+from eahub.localgroups.models import LocalGroup, Organisership
 
 
 class LocalGroupTestCase(TestCase):
     def test_organisers_names(self):
-        localGroup = LocalGroup()
-        localGroup.save()
+        local_group = LocalGroup()
+        local_group.save()
         user_without_profile = User()
         user_without_profile.save()
-        o = Organisership(user=user_without_profile, local_group=localGroup)
+        o = Organisership(user=user_without_profile, local_group=local_group)
         o.save()
 
-        organisers_names = localGroup.organisers_names()
+        organisers_names = local_group.organisers_names()
 
         self.assertEqual("User profile missing", organisers_names)
