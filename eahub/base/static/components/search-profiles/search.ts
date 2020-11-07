3461f1d6c2e687669e3aa9c5bcbc22286c7e5659
Victor Yunenko <victor@what.digital>
2020-11-07 18:36:58 +0200

    search: add mobile filters to navbar

--- eahub/base/static/components/search-profiles/search.ts
+++ eahub/base/static/components/search-profiles/search.ts
@@ -4,12 +4,17 @@ import {Vue, Component, Prop} from 'vue-property-decorator';
 import algoliasearch from 'algoliasearch/lite';
 import VueObserveVisibility from 'vue-observe-visibility';
 
+const SearchBox = require('./components/search-box.vue').default;
+
+
 
 Vue.use(VueObserveVisibility);
 
 
 @Component({
-    components: {},
+    components: {
+        SearchBox
+    },
 })
 export default class SearchComponent extends Vue {
     @Prop(String) algoliaApiKey: string;
