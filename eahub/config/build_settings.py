5633fbc46cd6be3b37b00125a14bf2d9e2531215
Sebastian <sebbecker@gmx.net>
2020-11-08 15:22:59 +0000

    Enable filtering of users by date added, last login and publicity on admin panel (#996)

--- eahub/config/build_settings.py
+++ eahub/config/build_settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     "eahub.base.apps.BaseConfig",
     "eahub.localgroups.apps.LocalGroupsConfig",
     "eahub.profiles.apps.ProfilesConfig",
+    "rangefilter",
 ]
 
 # Core settings: security
