9e0dd24c81a646aa5b486dca687c18f5ecb290e9
Victor Yunenko <victor@what.digital>
2020-12-06 13:47:40 +0200

    disable maintenance mode from master branch #984

--- eahub/config/settings.py
+++ eahub/config/settings.py
@@ -87,7 +87,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "maintenance_mode.middleware.MaintenanceModeMiddleware",
 ]
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
