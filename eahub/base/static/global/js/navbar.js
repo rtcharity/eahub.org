a3315a15af4f49833a8ceba5e17090750f65993d
Sebastian <sebbecker@gmx.net>
2020-12-06 10:23:46 +0000

    1007 update navbar (#1021)
    * add toggle icon
    
    * make menu similar
    
    * replicate new navbar for desktop
    
    * use flex
    
    * replicate new navbar for mobile
    
    * show groups link on profiles page
    
    * hide icons of pages, groups, my_profiles not if on them

--- eahub/base/static/global/js/navbar.js
+++ eahub/base/static/global/js/navbar.js
@@ -1,30 +1,49 @@
 export function initNavbar() {
-  const navbar = new Navbar($('#burger-btn'), $('#navbar'));
+  const navbar = new Navbar($('#menu-toggle-icon'), $('#burger-btn'), $('#dropdown'), $('#navbar-collapsable'));
   navbar.toggleMenuOnClick();
   navbar.disappearMenuOnMovingCursorAway();
 }
 
 class Navbar {
-  constructor(burgerBtnHtmlElement, navbarHtmlElement) {
-    this.burgerBtnHtmlElement = burgerBtnHtmlElement;
-    this.navbarHtmlElement = navbarHtmlElement;
+  constructor(navbarToggleIcon, burgerBtn, dropdownElement, navbarCollapsable) {
+    this.navbarToggleIcon = navbarToggleIcon;
+    this.dropdownElement = dropdownElement;
+    this.burgerBtn = burgerBtn;
+    this.navbarCollapsable = navbarCollapsable;
   }
 
   toggleMenuOnClick() {
     const that = this;
-    this.burgerBtnHtmlElement.click(function(e) {
-      e.stopImmediatePropagation();
-      that.navbarHtmlElement.toggle();
-    })
+    this.navbarToggleIcon.click(function(e) {that.toggleMenu(e)})
+    this.burgerBtn.click(function(e) {that.toggleCollapsable(e)})
   }
 
   disappearMenuOnMovingCursorAway() {
-    this.navbarHtmlElement.onmouseout = function(event) {
+    this.dropdownElement.onmouseout = function(event) {
       var element_left = event.target
       var element_new = event.relatedTarget
       if (element_new.className.includes('container') || element_new.id == 'body') {
         navbar.style.display = 'none'
       }
     }
+  }
+
+  toggleMenu(e) {
+    var that = this;
+    var classList = that.navbarToggleIcon[0].classList;
+    if (classList.contains("menu-toggle-up")) {
+        classList.remove("menu-toggle-up")
+        classList.add("menu-toggle-down")
+    } else {
+        classList.remove("menu-toggle-down")
+        classList.add("menu-toggle-up")
+    }
+    e.stopImmediatePropagation();
+    that.dropdownElement.toggle();
+  }
+
+  toggleCollapsable(e) {
+    e.stopImmediatePropagation();
+    this.navbarCollapsable.toggle();
   }
 }
