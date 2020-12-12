52017db493d9f92521b61bc022a85a6718c52547
Sebastian <sebbecker@gmx.net>
2020-12-12 17:17:10 +0000

    to handle group types import add customised widget for enum array

--- eahub/localgroups/admin.py
+++ eahub/localgroups/admin.py
@@ -1,14 +1,36 @@
 from typing import List, Optional
 
 from django.contrib import admin
+from django.contrib.postgres.fields import ArrayField
 from import_export import fields
 from import_export.admin import ImportExportMixin
 from import_export.resources import ModelResource
 from import_export.widgets import ManyToManyWidget
+from import_export import widgets
 
 from eahub.base.models import User
 from eahub.base.utils import ExportCsvMixin
 from eahub.localgroups.models import LocalGroup, LocalGroupType
+
+import ipdb;
+
+class EnumArrayWidget(widgets.Widget):
+    """
+    Widget for an Array field. Can be used for Postgres' Array field.
+    :param separator: Defaults to ``','``
+    """
+
+    def __init__(self, separator=None):
+        if separator is None:
+            separator = ','
+        self.separator = separator
+        super().__init__()
+
+    def clean(self, value, row=None, *args, **kwargs):
+        return value
+
+    def render(self, value, obj=None):
+        return self.separator.join(str(v) for v in value)
 
 
 class LocalGroupResource(ModelResource):
@@ -29,7 +51,6 @@ class LocalGroupResource(ModelResource):
             "organisers",
             "organisers_freetext",
             "email",
-            "local_group_type_dehydrated",
             "local_group_types_dehydrated",
             "city_or_town",
             "country",
@@ -45,8 +66,14 @@ class LocalGroupResource(ModelResource):
             "other_info",
         ]
 
+    @classmethod
+    def widget_from_django_field(cls, f, default=widgets.Widget):
+        if isinstance(f, ArrayField):
+            return EnumArrayWidget
+        else:
+            return super().widget_from_django_field(f)
+
     def before_import_row(self, row: dict, **kwargs) -> dict:
-        row["local_group_type"] = self.hydrate_local_group_type(row["type"])
         row["local_group_types"] = self.hydrate_local_group_types(row["types"])
         return super().before_import_row(row, **kwargs)
 
@@ -72,8 +99,9 @@ class LocalGroupResource(ModelResource):
         self, group_types_raw: str
     ) -> Optional[List[LocalGroupType]]:
         group_types = []
-        for group_type_raw in group_types_raw:
+        for group_type_raw in group_types_raw.split(","):
             group_types.append(self.hydrate_local_group_type(group_type_raw))
+
         return group_types if group_types else None
 
 
@@ -122,3 +150,5 @@ class LocalGroupAdmin(ImportExportMixin, admin.ModelAdmin, ExportCsvMixin):
         return ExportCsvMixin.export_csv(
             self, request, queryset, fieldnames, "localgroups"
         )
+
+
