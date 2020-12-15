3829338918ca2d5b4e1bfcfabfbb1551f90a3c57
Sebastian <sebbecker@gmx.net>
2020-12-15 21:39:54 +0000

    Fix bug that exported profile fields in wrong order

--- eahub/base/utils.py
+++ eahub/base/utils.py
@@ -14,11 +14,12 @@ def user_display(user):
 
 
 class ExportCsvMixin:
-    def export_csv(self, request, queryset, field_names, filename):
+    def export_csv(self, request, queryset, model, filename):
         response = HttpResponse(content_type="text/csv")
         response["Content-Disposition"] = "attachment; filename={}.csv".format(filename)
         writer = csv.writer(response)
 
+        field_names = model.get_exportable_field_names()
         writer.writerow(field_names)
         for obj in queryset:
             writer.writerow(obj.convert_to_row(field_names))
