b8c740ddcbbdcee4bdd76a0ab8b6f697aece17dc
victor-yunenko <victor@what.digital>
2020-12-19 17:26:34 +0200

    Add a separate algolia index for testing, fix cause_areas or careers update bug, optimize collectstatic and docker (#1060)

--- eahub/config/build_settings.py
+++ eahub/config/build_settings.py
@@ -44,5 +44,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 ALGOLIA = {
     "APPLICATION_ID": env.str("ALGOLIA_APPLICATION_ID", default="PFD0UVG9YB"),
-    "API_KEY": env.str("ALGOLIA_API_KEY", default=""),
+    "API_KEY": env.str("ALGOLIA_API_KEY", default="d1c9139b2271e35f44a29b12dddb4b06"),
+    "API_KEY_READ_ONLY": env.str("API_KEY_READ_ONLY", default="1e1434756fb69ac8fcddd9fcc0ffcdc4"),
+    "INDEX_NAME_PROFILES": env.str("ALGOLIA_INDEX_NAME_PROFILES", default="profiles_stage"),
 }
--- eahub/profiles/models.py
+++ eahub/profiles/models.py
@@ -4,6 +4,7 @@ import pathlib
 import shutil
 import zipfile
 from typing import List, Optional
+from typing import Union
 
 from django import urls
 from django.conf import settings
@@ -530,9 +531,23 @@ class Profile(models.Model):
         return values
 
     def _format_enum_array_for_searching(
-        self, array: List[enum.Enum], enum_cls: enum.Enum
+        self,
+        enum_values_list: List[Union[enum.Enum, str, int]],
+        enum_cls: enum.Enum,
     ) -> List[str]:
-        return [item[1] for item in enum_cls.choices() if item[0] in array]
+        enum_labels: List[str] = []
+        for enum_value_raw in enum_values_list:
+            enum_value = int(enum_value_raw)
+            enum_labels.append(enum_cls.values[enum_value].label)
+        return enum_labels
+
+    @staticmethod
+    def get_exportable_field_names():
+        return [
+            field.name
+            for field in Profile._meta.fields + Profile._meta.many_to_many
+            if "_other" not in field.name
+        ]
 
     @staticmethod
     def get_exportable_field_names():
