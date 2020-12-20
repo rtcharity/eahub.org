3e5994d654d2146a6933ea572d27e5118f7e5ba0
Victor Yunenko <victor@what.digital>
2020-12-20 18:27:54 +0200

    black: format code

--- eahub/config/settings.py
+++ eahub/config/settings.py
@@ -196,8 +196,12 @@ ACCOUNT_USERNAME_REQUIRED = False
 ALGOLIA = {
     "APPLICATION_ID": env.str("ALGOLIA_APPLICATION_ID", default="PFD0UVG9YB"),
     "API_KEY": env.str("ALGOLIA_API_KEY", default="d1c9139b2271e35f44a29b12dddb4b06"),
-    "API_KEY_READ_ONLY": env.str("API_KEY_READ_ONLY", default="19fd60051efeddf42e707383bf2f15a7"),
-    "INDEX_NAME_PROFILES": env.str("ALGOLIA_INDEX_NAME_PROFILES", default="profiles_stage"),
+    "API_KEY_READ_ONLY": env.str(
+        "API_KEY_READ_ONLY", default="19fd60051efeddf42e707383bf2f15a7"
+    ),
+    "INDEX_NAME_PROFILES": env.str(
+        "ALGOLIA_INDEX_NAME_PROFILES", default="profiles_stage"
+    ),
 }
 
 # Django reCAPTCHA
