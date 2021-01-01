5ad32fd10c94d1bc18e19f846fcd29d89cda1a02
Sebastian <sebbecker@gmx.net>
2021-01-01 19:57:57 +0000

    Add group updates to profile logs

--- eahub/profiles/views.py
+++ eahub/profiles/views.py
@@ -25,6 +25,7 @@ from .models import (
     Membership,
     OrganisationalAffiliation,
     Profile,
+    ProfileAnalyticsLog,
     ProfileSlug,
 )
 
@@ -198,18 +199,37 @@ def edit_profile_community(request):
     if request.method == "POST":
         form = EditProfileCommunityForm(request.POST, instance=request.user.profile)
         if form.is_valid():
+            old_local_groups = [
+                x.name
+                for x in Profile.objects.filter(id=request.user.profile.id)[
+                    0
+                ].local_groups.all()
+            ]
             profile = form.save(commit=False)
             profile.local_groups.clear()
             organisational_affiliations = request.POST.getlist(
                 "organisational_affiliations"
             )
-            profile.organisational_affiliations = organisational_affiliations
+            profile.organisational_affiliations = [
+                int(x) for x in organisational_affiliations
+            ]
             profile.save()
             group_affiliations = request.POST.getlist("local_groups")
             local_groups = LocalGroup.objects.filter(id__in=group_affiliations)
+
             for group in local_groups:
                 membership = Membership(profile=profile, local_group=group)
                 membership.save()
+
+            log = ProfileAnalyticsLog()
+            log.store(
+                request.user.profile,
+                "Update",
+                "local_groups",
+                old_local_groups,
+                [x.name for x in local_groups.all()],
+            )
+
             return redirect("my_profile")
     else:
         form = EditProfileCommunityForm(instance=request.user.profile)
