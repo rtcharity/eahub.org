4834bb3d57766149458d0c42c31495010ef3878f
victor-yunenko <victor@what.digital>
2021-01-03 15:16:16 +0000

    algolia: fix profiles pagination bug (#1088)
    Co-authored-by: Sebastian <sebbecker@gmx.net>

--- eahub/base/static/components/search-profiles/search.ts
+++ eahub/base/static/components/search-profiles/search.ts
@@ -1,14 +1,13 @@
 import {SearchClient} from 'algoliasearch/dist/algoliasearch-lite';
-import {Provide} from 'vue-property-decorator';
-import {Vue, Component, Prop} from 'vue-property-decorator';
 import algoliasearch from 'algoliasearch/lite';
+import {history as historyRouter} from 'instantsearch.js/es/lib/routers';
+import {singleIndex as singleIndexMapping} from 'instantsearch.js/es/lib/stateMappings';
 import VueObserveVisibility from 'vue-observe-visibility';
-import { history as historyRouter } from 'instantsearch.js/es/lib/routers';
-import { singleIndex as singleIndexMapping } from 'instantsearch.js/es/lib/stateMappings';
+import {Provide} from 'vue-property-decorator';
+import {Component, Prop, Vue} from 'vue-property-decorator';
 
 
 const SearchFilters = require('./components/search-filters.vue').default;
-
 
 
 Vue.use(VueObserveVisibility);
@@ -29,7 +28,19 @@ export default class SearchComponent extends Vue {
     @Provide() searchClient: SearchClient = algoliasearch(this.algoliaApplicationId, this.algoliaApiKey);
     @Provide() searchQuery: string = '';
     @Provide() routing = {
-        router: historyRouter(),
+        router: historyRouter({
+            // an exact copy of the default createURL that drops pagination
+            // and thus fixes #1034 
+            // original code: https://github.com/algolia/instantsearch.js/blob/718bf458152bb55bab1efb542adb8e31298c0c3c/src/lib/routers/history.ts#L29
+            createURL: ({qsModule, routeState, location}) => {
+                delete routeState.page;
+
+                const {protocol, hostname, port = '', pathname, hash} = location;
+                const queryString = qsModule.stringify(routeState);
+                const portWithPrefix = port === '' ? '' : `:${port}`;
+                return `${protocol}//${hostname}${portWithPrefix}${pathname}?${queryString}${hash}`;
+            },
+        }),
         stateMapping: singleIndexMapping(this.algoliaIndex),
     }
     @Provide() profileEmptyImages: URL[] = [];
