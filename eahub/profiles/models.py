3c3451c5163b4a472c84edcdc11a35b177ce3c1a
mattg95 <mattgoodman95@gmail.com@>
2021-01-04 10:31:28 +0000

    add orgs

--- eahub/profiles/models.py
+++ eahub/profiles/models.py
@@ -148,13 +148,11 @@ class GivingPledge(enum.Enum):
 class OrganisationalAffiliation(enum.Enum):
 
     EIGHTY_THOUSAND_HOURS = 1
-    ALLFED = 32
     ANIMAL_CHARITY_EVALUATORS = 2
     BERKELEY_EXISTENTIAL_RISK_INITIATIVE = 3
     CENTER_FOR_APPLIED_RATIONALITY = 4
     CENTER_FOR_HUMAN_COMPATIBLE_AI = 5
     CENTRE_FOR_EFFECTIVE_ALTRUISM = 6
-    CENTRE_ON_LONG_TERM_RISK = 34
     CSER = 7
     CHARITY_ENTREPRENEURSHIP = 8
     CHARITY_SCIENCE_HEALTH = 9
@@ -180,7 +178,9 @@ class OrganisationalAffiliation(enum.Enum):
     THE_GOOD_FOOD_INSTITUTE = 29
     THE_LIFE_YOU_CAN_SAVE = 30
     WILD_ANIMAL_INITIATIVE = 31
+    ALLFED = 32
     FUTURE_OF_HUMANITY_INSTITUTE = 33
+    CENTRE_ON_LONG_TERM_RISK = 34
     ANIMAL_ETHICS = 35
     GIVING_WHAT_WE_CAN = 36
     LESS_WRONG = 37
@@ -193,54 +193,148 @@ class OrganisationalAffiliation(enum.Enum):
     SENTIENCE_POLITICS = 44
     WANBAM = 45
     E_A_HUB = 46
+    ANIMAL_ADVOCACY_CAREERS = 47
+    AYUDA_EFECTIVA = 48
+    DOEBEM = 49
+    DONATIONAL = 50
+    EFFECTIVE_ALTRUISM_FOUNDATION = 51
+    EFFECTIVE_GIVING_UK_NETHERLANDS = 52
+    EFFECTIV_SPENDEN = 53
+    GENERATION_PLEDGE = 54
+    GIEFFEKTIVT = 55
+    GIVING_MULTIPLIER = 56
+    HIGH_IMPACT_ATHLETES = 57
+    LETS_FUND = 58
+    SO_GIVE = 59
+    ALPENGLOW = 60
+    PROJEKT_FRAMTID = 61
+    CENTRE_FOR_ELECTION_SCIENCE = 62
+    ALBERT_SCHWEITZER_FOUNDATION = 63
+    ANIMA_INTERNATIONAL = 64
+    ANIMAL_ASK = 65
+    AQUATIC_LIFE_INSTITUTE = 66
+    CREDENCE_INSTITUTE = 67
+    FARMED_ANIMAL_FUNDERS = 68
+    FAUNALYTICS = 69
+    FISH_WELFARE_INITIATIVE = 70
+    THE_HUMANE_LEAGUE = 71
+    AGAINST_MALARIA_FOUNDATION = 72
+    CANOPIE = 73
+    END_FUND = 74
+    EVIDENCE_ACTION = 75
+    FAMILY_EMPOWERMENT_MEDIA = 76
+    FORTIFY_HEALTH = 77
+    GIVEDIRECTLY = 78
+    HELEN_KELLER_INTERNATIONAL_VITAMIN_A = 79
+    IDINSIGHT = 80
+    LEAD_EXPOSURE_ELIMINATION_PROJECT = 81
+    MALARIA_CONSORTIUM = 82
+    NEW_INCENTIVES = 83
+    POLICY_ENTREPRENEURSHIP_NETWORK = 84
+    SCI_FOUNDATION = 85
+    SIGHTSAVERS_DEWORMING_PROGRAMME = 86
+    SUVITA = 87
+    OPIS = 88
+    OUR_WORLD_IN_DATA = 89
+    QUALIA_RESEARCH_INSTITUTE = 90
+    GIVING_GREEN = 91
+    HIGH_IMPACT_CAREERS_IN_GOVERNMENT = 92
+    SPARKWAVE = 93
 
     labels = {
         EIGHTY_THOUSAND_HOURS: "80,000 Hours",
-        ALLFED: "ALLFED",
-        ANIMAL_CHARITY_EVALUATORS: "Animal Charity Evaluators",
-        BERKELEY_EXISTENTIAL_RISK_INITIATIVE: "Berkeley Existential Risk Initiative",
-        CENTER_FOR_APPLIED_RATIONALITY: "Center for Applied Rationality",
-        CENTER_FOR_HUMAN_COMPATIBLE_AI: "Center for Human Compatible AI",
-        CENTRE_FOR_EFFECTIVE_ALTRUISM: "Centre for Effective Altruism",
-        CSER: "Centre for the Study of Existential Risk",
+        ANIMAL_CHARITY_EVALUATORS: "Animal Charity Evaluators (ACE)",
+        BERKELEY_EXISTENTIAL_RISK_INITIATIVE: "Berkeley Existential Risk Initiative (BERI)",
+        CENTER_FOR_APPLIED_RATIONALITY: "Center for Applied Rationality (CFAR)",
+        CENTER_FOR_HUMAN_COMPATIBLE_AI: "Center for Human Compatible AI (CHAI)",
+        CENTRE_FOR_EFFECTIVE_ALTRUISM: "Centre for Effective Altruism (CEA)",
+        CSER: "Centre for the Study of Existential Risk (CSER)",
         CHARITY_ENTREPRENEURSHIP: "Charity Entrepreneurship",
         CHARITY_SCIENCE_HEALTH: "Charity Science Health",
-        FORETHOUGHT_FOUNDATION: "Forethought Foundation",
-        FOUNDATIONAL_RESEARCH_INSTITUTE: "Foundational Research Institute",
+        FORETHOUGHT_FOUNDATION: "The Forethought Foundation For Global Priorities Research",
+        FOUNDATIONAL_RESEARCH_INSTITUTE: "Foundational Research Institute (FRI)",
         FOUNDERS_PLEDGE: "Founders Pledge",
-        FUTURE_OF_LIFE_INSTITUTE: "Future of Life Institute",
+        FUTURE_OF_LIFE_INSTITUTE: "Future of Life Institute (FLI)",
         GIVEWELL: "GiveWell",
-        GLOBAL_CATASTROPHIC_RISK_INSTITUTE: "Global Catastrophic Risk Institute",
-        GLOBAL_PRIORITIES_INSTITUTE: "Global Priorities Institute",
-        LCFI: "Leverhulme Center for the Future of Intelligence",
-        LOCAL_EFFECTIVE_ALTRUISM_NETWORK: "Local Effective Altruism Network",
-        MIRI: "Machine Intelligence Research Institute",
+        GLOBAL_CATASTROPHIC_RISK_INSTITUTE: "Global Catastrophic Risk Institute (GCRI)",
+        GLOBAL_PRIORITIES_INSTITUTE: "Global Priorities Institute (GPI)",
+        LCFI: "Leverhulme Center for the Future of Intelligence (CFI)",
+        LOCAL_EFFECTIVE_ALTRUISM_NETWORK: "Local Effective Altruism Network (LEAN)",
+        MIRI: "Machine Intelligence Research Institute (MIRI)",
         ONE_FOR_THE_WORLD: "One for the World",
         OPEN_PHILANTHROPY_PROJECT: "Open Philanthropy Project",
-        RAISING_FOR_EFFECTIVE_GIVING: "Raising for Effective Giving",
+        RAISING_FOR_EFFECTIVE_GIVING: "Raising for Effective Giving (REG)",
         RETHINK_CHARITY: "Rethink Charity",
         RETHINK_CHARITY_FORWARD: "Rethink Charity Forward",
         RETHINK_PRIORITIES: "Rethink Priorities",
         SENTIENCE_INSTITUTE: "Sentience Institute",
-        STIFTUNG_FUR_EFFEKTIVEN_ALTRUISMUS: "Stiftung für Effektiven Altruismus (EAF)",
+        STIFTUNG_FUR_EFFEKTIVEN_ALTRUISMUS: "Stiftung für Effektiven Altruismus",
         STUDENTS_FOR_HIGH_IMPACT_CHARITY: "Students for High Impact Charity",
         THE_GOOD_FOOD_INSTITUTE: "The Good Food Institute",
-        THE_LIFE_YOU_CAN_SAVE: "The Life You Can Save",
+        THE_LIFE_YOU_CAN_SAVE: "The Life You Can Save (TLYCS)",
         WILD_ANIMAL_INITIATIVE: "Wild Animal Initiative",
-        FUTURE_OF_HUMANITY_INSTITUTE: "Future of Humanity Institute",
-        CENTRE_ON_LONG_TERM_RISK: "Centre on Long-Term Risk",
+        ALLFED: "Alliance to Feed the Earth in Disasters (ALLFED)",
+        FUTURE_OF_HUMANITY_INSTITUTE: "Future of Humanity Institute (FHI)",
+        CENTRE_ON_LONG_TERM_RISK: "Centre on Long-Term Risk (CLR)",
         ANIMAL_ETHICS: "Animal Ethics",
-        GIVING_WHAT_WE_CAN: "Giving What We Can",
+        GIVING_WHAT_WE_CAN: "Giving What We Can (GWWC)",
         LESS_WRONG: "Less Wrong",
         HAPPIER_LIVES_INSTITUTE: "Happier Lives Institute",
-        INNOVATIONS_FOR_POVERTY_ACTION: "Innovations for Poverty Action",
-        JPAL: "J-PAL",
+        INNOVATIONS_FOR_POVERTY_ACTION: "Innovations for Poverty Action (IPA)",
+        JPAL: "Abdul Latif Jameel Poverty Action Lab (J-PAL)",
         APPG_FUTURE_GENERATIONS: "All-Party Parliamentary Group for Future Generations",
-        CEEALAR: "Centre for Enabling EA Research and Learning",
+        CEEALAR: "Centre for Enabling EA Research and Learning (CEEALAR)",
         LEGAL_PRIORITIES_PROJECT: "Legal Priorities Project",
         SENTIENCE_POLITICS: "Sentience Politics",
         WANBAM: "WANBAM",
-        E_A_HUB: "Effective Alturism Hub"
+        E_A_HUB: "Effective Alturism Hub",
+        ANIMAL_ADVOCACY_CAREERS: "Animal Advocacy Careers",
+        AYUDA_EFECTIVA: "Ayuda Efectiva",
+        DOEBEM: "Doebem",
+        DONATIONAL: "Donational",
+        EFFECTIVE_ALTRUISM_FOUNDATION: "Effective Altruism Foundation (EAF)",
+        EFFECTIVE_GIVING_UK_NETHERLANDS: "Effective Giving UK/Netherlands",
+        EFFECTIV_SPENDEN: "Effektiv-Spenden.org",
+        GENERATION_PLEDGE: "Generation Pledge",
+        GIEFFEKTIVT: "GiEffektivt.No",
+        GIVING_MULTIPLIER: "Giving Multiplier.org",
+        HIGH_IMPACT_ATHLETES: "High Impact Athletes",
+        LETS_FUND: "Let's Fund",
+        SO_GIVE: "So Give",
+        ALPENGLOW: "Alpenglow",
+        PROJEKT_FRAMTID: "Projekt Framtid",
+        CENTRE_FOR_ELECTION_SCIENCE: "Centre for Election Science (CES)",
+        ALBERT_SCHWEITZER_FOUNDATION: "Albert Schweitzer Foundation",
+        ANIMA_INTERNATIONAL: "Anima International",
+        ANIMAL_ASK: "Animal Ask",
+        AQUATIC_LIFE_INSTITUTE: "Aquatic Life Institute",
+        CREDENCE_INSTITUTE: "Credence Institute",
+        FARMED_ANIMAL_FUNDERS: "Farmed Animal Funders",
+        FAUNALYTICS: "Faunalytics",
+        FISH_WELFARE_INITIATIVE: "Fish Welfare Initiative",
+        THE_HUMANE_LEAGUE: "The Humane League",
+        AGAINST_MALARIA_FOUNDATION: "Against Malaria Foundation (AMF)",
+        CANOPIE: "Canopie",
+        END_FUND: "END Fund (deworming programme)",
+        EVIDENCE_ACTION: "Evidence Action (Deworm the World Initiative programme)",
+        FORTIFY_HEALTH: "Fortify Health",
+        GIVEDIRECTLY: "GiveDirectly",
+        HELEN_KELLER_INTERNATIONAL_VITAMIN_A: "Hellen Keller International (Vitamin A supplementation programme)",
+        IDINSIGHT: "IDInsight",
+        FAMILY_EMPOWERMENT_MEDIA: "Family Empowerment Media",
+        LEAD_EXPOSURE_ELIMINATION_PROJECT: "Lead Exposure Elimination Project (LEEP)",
+        MALARIA_CONSORTIUM: "Malaria Consortium",
+        NEW_INCENTIVES: "New Incentives",
+        POLICY_ENTREPRENEURSHIP_NETWORK: "Policy Entrepreneurship Network",
+        SCI_FOUNDATION: "SCI Foundation",
+        SIGHTSAVERS_DEWORMING_PROGRAMME: "Sightsavers (deworming programme)",
+        SUVITA: "Suvita",
+        OPIS: "Organisation for the Prevention of Intense Suffering (OPIS)",
+        OUR_WORLD_IN_DATA: "Our World in Data",
+        QUALIA_RESEARCH_INSTITUTE: "Qualia Research Institute (QRI)",
+        GIVING_GREEN: "Giving Green",
+        HIGH_IMPACT_CAREERS_IN_GOVERNMENT: "High Impact Careers in Government (HIPE)",
+        SPARKWAVE: "SparkWave"
     }
 
 
