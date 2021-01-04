f09f22bf96281456ddb4b516ee5fb7b7d1f6fa4b
mattg95 <mattgoodman95@gmail.com@>
2021-01-04 11:26:15 +0000

    change names to fix flake8 line length overrun

--- eahub/profiles/views.py
+++ eahub/profiles/views.py
@@ -191,6 +191,7 @@ def edit_profile_career(request):
         },
     )
 
+
 def reorder_orgs(orgs):
     return sorted(orgs, key=lambda x: x[1].label)
 
@@ -242,7 +243,7 @@ def edit_profile_community(request):
         {
             "form": form,
             "profile": Profile.objects.get(pk=request.user.profile.id),
-            "organisational_affiliation_choices": reorder_orgs(OrganisationalAffiliation.choices()),
+            "organisation_choices": reorder_orgs(OrganisationalAffiliation.choices()),
         },
     )
 
