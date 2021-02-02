979f78bd9169140f4b37c7d71d63cb747b45beed
Victor Yunenko <victor@what.digital>
2021-02-02 20:07:25 +0000

    settings: tweak ALLOWED_HOSTS by add default for docker build

--- eahub/config/settings.py
+++ eahub/config/settings.py
@@ -127,7 +127,7 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-ALLOWED_HOSTS = env.list("HOSTS") + ["127.0.0.1", "*"]
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[]) + ["127.0.0.1", "*"]
 
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
