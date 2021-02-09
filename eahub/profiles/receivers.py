ba95f4781c633cacb0fff35ee728060ae4652658
victor-yunenko <victor@what.digital>
2021-02-09 16:49:32 +0000

    Log user updates + add simple fixtures generation (#1151)
    * log user updates + add fixtures Generator
    
    * log user updates: hide passwords
    
    * log user updates: prevent user flow disruption
    
    * log user updates: allow email logs

--- eahub/profiles/receivers.py
+++ eahub/profiles/receivers.py
@@ -6,6 +6,7 @@ from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from django.utils import timezone
 
+from eahub.base.models import User
 from eahub.profiles.models import (
     CauseArea,
     ExpertiseArea,
@@ -26,7 +27,7 @@ profile_fields_enums_map = {
 }
 
 
-def save_logs_for_new_profile(instance):
+def save_logs_for_new_profile(instance: Profile):
     action_uuid = uuid.uuid4()
     time = timezone.now()
     for field in instance._meta.get_fields():
@@ -48,26 +49,51 @@ def save_logs_for_new_profile(instance):
             log.save()
 
 
-def save_logs_for_profile_update(instance, previous):
-    new_fields = instance._meta.get_fields()
+def save_logs_for_profile_update(instance_new: Profile, instance_old: Profile):
     action_uuid = uuid.uuid4()
     time = timezone.now()
-    for field in new_fields:
+    for field in instance_new._meta.get_fields():
         try:
-            old_value = getattr(previous, field.name)
-            value = getattr(instance, field.name)
+            value_new = getattr(instance_new, field.name)
+            value_old = getattr(instance_old, field.name)
         except AttributeError:
             continue
-        if value != old_value:
+        if value_new != value_old:
             log = ProfileAnalyticsLog()
-            log.profile = instance
+            log.profile = instance_new
             log.field = field.name
             log.action = "Update"
-            log.old_value = convert_value_to_printable(old_value, field.name)
-            log.new_value = convert_value_to_printable(value, field.name)
+            log.old_value = convert_value_to_printable(value_old, field.name)
+            log.new_value = convert_value_to_printable(value_new, field.name)
             log.time = time
             log.action_uuid = action_uuid
             log.save()
+
+
+def _save_logs_for_profile_user_update(user_old: User, user_new: User):
+    action_uuid = uuid.uuid4()
+    time = timezone.now()
+    for field in user_new._meta.get_fields():
+        try:
+            value_new = getattr(user_new, field.name)
+            value_old = getattr(user_old, field.name)
+        except AttributeError:
+            continue
+        if value_new != value_old:
+            is_must_protect_password = field.name == "password"
+            if is_must_protect_password and value_old == "":
+                value_old_formatted = ""
+            else:
+                value_old_formatted = "[protected]"
+            ProfileAnalyticsLog.objects.create(
+                action_uuid=action_uuid,
+                time=time,
+                action="Update",
+                old_value=value_old_formatted,
+                new_value="[protected]" if is_must_protect_password else value_new,
+                profile=user_new.profile,
+                field=field.name,
+            )
 
 
 def convert_value_to_printable(value, field):
@@ -91,13 +117,33 @@ def clear_the_cache(**kwargs):
 
 @receiver(post_save, sender=Profile)
 def on_profile_creation(**kwargs):
-    if "created" in kwargs.keys() and kwargs["created"]:
-        save_logs_for_new_profile(kwargs["instance"])
+    try:
+        if "created" in kwargs.keys() and kwargs["created"]:
+            save_logs_for_new_profile(kwargs["instance"])
+    except:
+        logger.exception("Profile creation logging failed")
 
 
 @receiver(pre_save, sender=Profile)
 def on_profile_change(**kwargs):
-    instance = kwargs["instance"]
-    if instance.id is not None:
-        previous = Profile.objects.get(id=instance.id)
-        save_logs_for_profile_update(instance, previous)
+    try:
+        instance = kwargs["instance"]
+        if instance.id is not None:
+            previous = Profile.objects.get(id=instance.id)
+            save_logs_for_profile_update(instance, previous)
+    except:
+        logger.exception("Profile update logging failed")
+
+
+@receiver(pre_save, sender=User)
+def on_user_change(**kwargs):
+    try:
+        user_new: User = kwargs["instance"]
+        is_change_action = user_new.id is not None
+        if is_change_action and user_new.has_profile():
+            _save_logs_for_profile_user_update(
+                user_old=User.objects.get(id=user_new.id),
+                user_new=user_new,
+            )
+    except:
+        logger.exception("User update logging failed")
