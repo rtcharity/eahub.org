473c461dd5d02ed3993314898248a50e0c68b6f3
Victor Yunenko <victor@what.digital>
2021-03-27 13:12:03 +0000

    improve var naming

--- eahub/profiles/views.py
+++ eahub/profiles/views.py
@@ -27,7 +27,7 @@ def profile_detail_or_redirect(request: HttpRequest, slug: str) -> HttpResponse:
         template_name="eahub/profile.html",
         context={
             "profile": profile,
-            "is_cause_area_section": (
+            "is_render_cause_area_section": (
                 profile.tags_organisational_affiliation.exists()
                 or profile.tags_pledge.exists()
                 or profile.cause_areas_other
