969cf235a932a1aa17badd3e4566e2ab43e82864
Sebastian <sebbecker@gmx.net>
2021-04-21 08:13:21 +0100

    code review comments

--- eahub/profiles/tests/test_views.py
+++ eahub/profiles/tests/test_views.py
@@ -1,41 +1,38 @@
 from django.test import override_settings
+from django.urls import reverse
 
 from eahub.tests.cases import EAHubTestCase
 from eahub.profiles.models import VisibilityEnum
 
 
-@override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
+#@override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
 class ProfileTestCase(EAHubTestCase):
 
     def test_private_profile_hidden(self):
-        profile = self.gen.profile(first_name="first", last_name="last")
-        profile.visibility = VisibilityEnum.PRIVATE
+        profile = self.gen.profile(first_name="first", last_name="last", visibility=VisibilityEnum.PRIVATE)
         profile.save()
-        response = self.client.get('/profile/first-last/')
+        response = self.client.get(reverse("profiles_app:profile", args=([profile.slug])))
         self.assertEqual(response.status_code, 404)
 
     def test_internal_profile_hidden(self):
-        profile = self.gen.profile(first_name="first", last_name="last")
-        profile.visibility = VisibilityEnum.INTERNAL
+        profile = self.gen.profile(first_name="first", last_name="last", visibility=VisibilityEnum.INTERNAL)
         profile.save()
-        response = self.client.get('/profile/first-last/')
+        response = self.client.get(reverse("profiles_app:profile", args=([profile.slug])))
         self.assertEqual(response.status_code, 404)
 
     def test_internal_profile_visible_to_approved_user(self):
-        profile = self.gen.profile(first_name="first", last_name="last")
-        profile.visibility = VisibilityEnum.INTERNAL
+        profile = self.gen.profile(first_name="first", last_name="last", visibility=VisibilityEnum.INTERNAL)
         profile.save()
         profile_visitor = self.gen.profile()
         profile_visitor.is_approved = True
         self.client.force_login(profile_visitor.user)
 
-        response = self.client.get('/profile/first-last/')
+        response = self.client.get(reverse("profiles_app:profile", args=([profile.slug])))
         self.assertEqual(response.status_code, 200)
 
     def test_public_profile_visible(self):
-        profile = self.gen.profile(first_name="first", last_name="last")
-        profile.visibility = VisibilityEnum.PUBLIC
+        profile = self.gen.profile(first_name="first", last_name="last", visibility=VisibilityEnum.PUBLIC)
         profile.save()
-        response = self.client.get('/profile/first-last/')
+        response = self.client.get(reverse("profiles_app:profile", args=([profile.slug])))
         self.assertEqual(response.status_code, 200)
 
