66ad823fef278e4710b6459bf3c3714a324eb4d3
Victor Yunenko <victor@what.digital>
2021-05-17 02:19:33 +0100

    add a mailchimp export EAG manage.py command

--- eahub/base/management/commands/eag_export_for_mailchimp.py
+++ eahub/base/management/commands/eag_export_for_mailchimp.py
@@ -6,36 +6,34 @@ from allauth.account.utils import user_pk_to_url_str
 from django.core.management import base
 from django.urls import reverse
 
-from eahub.base.models import User
+from eahub.profiles.models import Profile
 
 
 class Command(base.BaseCommand):
     def handle(self, *args, **options):
         df_mailchimp_raw: List[list] = []
         token_generator = EmailAwarePasswordResetTokenGenerator()
-        for date_str in ["03-29", "04-12"]:
-            chunks_count = 28 if date_str == "03-29" else 22
-            for index in range(0, chunks_count):
-                df = pandas.read_csv(f"data/users-{date_str}-filtered-part{index}.csv")
-                for _, row in df.iterrows():
-                    user = User.objects.get(email=row["user"])
-                    password_reset_link = reverse(
-                        "account_reset_password_from_key",
-                        kwargs=dict(
-                            uidb36=user_pk_to_url_str(user),
-                            key=token_generator.make_token(user),
-                        ),
-                    )
-                    df_mailchimp_raw.append(
-                        [
-                            row["user"],
-                            row["first_name"],
-                            row["last_name"],
-                            f"https://eahub.org{password_reset_link}",
-                            "EA Global Reconnect",
-                            "EAGR - 2021.05.17",
-                        ]
-                    )
+        with open("data/mailchimp-emails.csv") as file:
+            for line in file:
+                print(line)
+                profile = Profile.objects.get(user__email=line.strip())
+                password_reset_link = reverse(
+                    "account_reset_password_from_key",
+                    kwargs=dict(
+                        uidb36=user_pk_to_url_str(profile.user),
+                        key=token_generator.make_token(profile.user),
+                    ),
+                )
+                df_mailchimp_raw.append(
+                    [
+                        profile.user.email,
+                        profile.first_name,
+                        profile.last_name,
+                        f"https://eahub.org{password_reset_link}",
+                        "EA Global Reconnect",
+                        "EAGR - 2021.05.17",
+                    ]
+                )
         df = pandas.DataFrame(
             df_mailchimp_raw,
             columns=[
